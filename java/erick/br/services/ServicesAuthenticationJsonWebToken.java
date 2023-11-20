17bfb81f2a22b5b8128e511aab8c2450a296372b
erickbrunopequenodossantos@outlook.com <erickbrunopequenodossantos@outlook.com>
2023-11-20 20:52:15 -0300

    teste nova branch

--- java/erick/br/services/ServicesAuthenticationJsonWebToken.java
+++ java/erick/br/services/ServicesAuthenticationJsonWebToken.java
@@ -3,11 +3,17 @@ package erick.br.services;
 import java.io.IOException;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import erick.br.model.Usuario;
+import erick.br.repository.RepositoryUsuario;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("deprecation")
@@ -15,6 +21,8 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class ServicesAuthenticationJsonWebToken {
 
+	@Autowired
+	RepositoryUsuario repositoryUsuario;
 	private static final long TIME_EXPIRATION = 172800000;
 
 	private static final String SECRET_PASSWORD = "WgwbkKNQZZnmc69hgDwPoZ0XE1BcRYPDuKyQZoelidA=";
@@ -26,16 +34,37 @@ public class ServicesAuthenticationJsonWebToken {
 
 	public void authorizationToken(HttpServletResponse response, String userName) throws IOException {
 
-		String jwt = Jwts.builder() /*GERAR O TOKEN*/
-				.setSubject(userName) /*SETA O USUARIO*/
-				.setExpiration(new Date(System.currentTimeMillis() + TIME_EXPIRATION))/*PASSA O TEMPO LIMITE DE EXPiRACAO DO TOKEN*/
-				.signWith(SignatureAlgorithm.ES512, SECRET_PASSWORD).compact(); /*COMPACTA GERANDO A STRING DE AUTHENTICAO CRIPTOGRAFADA*/
+		String jwt = Jwts.builder() /* GERAR O TOKEN */
+				.setSubject(userName) /* SETA O USUARIO */
+				.setExpiration(new Date(System.currentTimeMillis() + TIME_EXPIRATION)) //tempo de expiracao 
+				.signWith(SignatureAlgorithm.ES512, SECRET_PASSWORD) // passar o algoritmo de criptografia + a senha secreta
+				.compact(); 
 
-		String token = TOKEN_PREFIX + "" + jwt; /*JUNTA O A CHAVE COM O VALOR */
-		response.setHeader(HEADER_RESPONSE, token);/*ESCREVE O TOKEN NO HEADER DE REPOSTA OU CABEÇALHO SE ASSIM QUEIRA CHAMAR*/
-		
+		String token = TOKEN_PREFIX + "" + jwt; /* JUNTA O A CHAVE COM O VALOR */
+		response.setHeader(HEADER_RESPONSE,token);/* ESCREVE O TOKEN NO HEADER DE REPOSTA OU CABEÇALHO SE ASSIM QUEIRA CHAMAR */
+
 		// GERAR RESPOSTA DO TOKEN EM JSON
-		response.getWriter().write("{\"Authorization\" : \""+token+"\"}");
+		response.getWriter().write("{\"Authorization\" : \"" + token + "\"}");
+	}
+
+	public Authentication getAuthentication(HttpServletRequest request) {
+
+		String token = request.getHeader(HEADER_RESPONSE);
+		if (token != null) {
+			String user = Jwts.parser().setSigningKey(SECRET_PASSWORD).parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+					.getBody().getSubject();
+
+			if (user != null && user.isEmpty()) {
+
+				Usuario userAutenticado = repositoryUsuario.findUserByLogin(user);
+				return new UsernamePasswordAuthenticationToken(
+						userAutenticado.getEmail(), 
+						userAutenticado.getSenha(),
+						userAutenticado.getAuthorities());
+			}
+		}
+		return null;
+
 	}
 
 }
